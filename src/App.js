78d8994cce1fd85c7d2b008503a18f8e86ec196a
Vladimir <maiboroda-vladimir@yandex.ru>
2019-01-11 16:36:21 +0300

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -3,29 +3,19 @@ import { Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components'
 import './App.css';
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="container">
-         <SayFullName name="Vladimir" surname="Mayboroda" link="https://maiboroda.pro" />
-         <SayFullName name="Ivan" surname="Ivanov" link="https://ivanov.pro" />
-         <SayFullName name="Sergey" surname="Sergeev" link="https://sergeev.pro" />
+        <div className="row">
+          <div className="col">
+            <h1>Hellow World!</h1>
+          </div> 
+        </div>
+        
       </div>
     );
   }
 }
-
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Мое имя {props.name}, фамилия - {props.surname}</h1>
-      <a href={props.link}> >> Ссылка на мой профиль</a>
-    </div>
-  )
-}
-
 
 export default App;
